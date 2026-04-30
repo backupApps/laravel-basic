65555bcfd758922240a386296cf34e6fc115327e
agoengBani <agungbani77@gmail.com>
2026-04-30 16:11:44 +0700

    create

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mahasiswa;
+use App\Models\Orangtua;
 use Illuminate\Http\Request;
 
 class FormController extends Controller
@@ -10,5 +12,32 @@ class FormController extends Controller
     public function index()
     {
         return view('form');
+    }
+
+    public function simpan(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'nim' => 'required',
+            'alamat' => 'required',
+            'nama_ayah' => 'required',
+            'nama_ibu' => 'required',
+        ]);
+
+        // simpan mahasiswa
+        $mahasiswa = Mahasiswa::create([
+            'nama' => $request->nama,
+            'nim' => $request->nim,
+            'alamat' => $request->alamat,
+        ]);
+
+        // simpan orangtua dari mahasiswa tersebut
+        Orangtua::create([
+            'mahasiswa_id' => $mahasiswa->id,
+            'nama_ayah' => $request->nama_ayah,
+            'nama_ibu' => $request->nama_ibu,
+        ]);
+
+        return redirect()->route('form')->with('sukses', 'Data mahasiswa berhasil ditambahkan.');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,10 @@ use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
 Route::get('form', [FormController::class, 'index'])->name('form');
+Route::post('form', [FormController::class, 'simpan'])->name('form.simpan');
+
 Route::get('tabel', [TableController::class, 'index'])->name('tabel');
+
+// HTTP Method -> get (ambil data), post (simpan data), put (update data), delete (hapus data), patch (update sebagian data), option
