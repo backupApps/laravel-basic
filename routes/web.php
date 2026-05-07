beb67f49e156dea54041d02c256199c263fb76d6
taa <user@MacBook-Air.local>
2026-05-07 16:38:37 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,15 @@ use App\Http\Controllers\TableController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-
+// form simpan
 Route::get('form', [FormController::class, 'index'])->name('form');
 Route::post('form', [FormController::class, 'simpan'])->name('form.simpan');
 
+// form edit
+Route::get('/form-edit/{id}', [FormController::class, 'edit'])->name('form.edit');
+Route::put('/form-update/{id}', [FormController::class, 'update'])->name('form.update');
+
+// tabel
 Route::get('tabel', [TableController::class, 'index'])->name('tabel');
 
 // HTTP Method -> get (ambil data), post (simpan data), put (update data), delete (hapus data), patch (update sebagian data), option
